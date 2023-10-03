470fdafc2494f80371d617d0992ddd7aa712b4cf
OnerCode <bsronipahm23@gmail.com>
2023-10-03 12:01:52 -0700

    committed task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(ord('a'), ord('z') + 1):
-     print("{}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
