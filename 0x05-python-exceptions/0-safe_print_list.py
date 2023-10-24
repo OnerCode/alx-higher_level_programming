fbb2b69b8c9ddf1cf217593f8b4a115b99e5835f
OnerCode <bsronipahm23@gmail.com>
2023-10-24 13:53:28 -0700

    Corrected task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for element in my_list:
             if count < x:
+
                 print(element, end=' ')
                 count += 1
-        print() 
-    except:
+        print()
+    except TypeError:
         pass
 
-    return 
+    return count
